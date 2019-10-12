d1fe046e9e0ad91eb08b782dc4da35097887161b
Mohammed Nasir <mnehaal@gmail.com>
2019-10-12 00:40:56 -0400

    Update values.py

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -207,7 +207,7 @@ DBC = {
   CAR.RAV4_TSS2: dbc_dict('toyota_nodsu_pt_generated', 'toyota_tss2_adas'),
   CAR.COROLLA_TSS2: dbc_dict('toyota_nodsu_pt_generated', 'toyota_tss2_adas'),
   CAR.LEXUS_ESH_TSS2: dbc_dict('toyota_nodsu_pt_generated', 'toyota_tss2_adas'),
-  CAR.LEXUS_ESH: dbc_dict('lexus_rx_hybrid_2017_pt_generated', 'toyota_adas'),
+  CAR.LEXUS_ESH: dbc_dict('lexus_es_hybrid_2018_pt_generated', 'toyota_adas'),
   CAR.SIENNA: dbc_dict('toyota_sienna_xle_2018_pt_generated', 'toyota_adas'),
 }
 

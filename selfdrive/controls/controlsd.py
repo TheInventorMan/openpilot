9bd0e0f3ee5fedc3248a54b04cae6d8978fc053e
Mohammed Nasir <mnehaal@gmail.com>
2019-12-31 17:57:25 -0500

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -578,7 +578,7 @@ def controlsd_thread(sm=None, pm=None, can_sock=None):
     # Create alerts
     if not sm.all_alive_and_valid():
       events.append(create_event('commIssue', [ET.NO_ENTRY, ET.SOFT_DISABLE]))
-      dummy_controlsd_logger(sm)
+      #dummy_controlsd_logger(sm)
     if not sm['pathPlan'].mpcSolutionValid:
       events.append(create_event('plannerError', [ET.NO_ENTRY, ET.IMMEDIATE_DISABLE]))
     if not sm['pathPlan'].sensorValid:
